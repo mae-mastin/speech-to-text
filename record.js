498b4c126fe402baaa9703e1b02c94e2c3a1ba5f
Chenwei-1999 <73069964+Chenwei-1999@users.noreply.github.com>
2022-11-01 08:55:36 -0500

    Add files via upload

--- record.js
+++ record.js
@@ -1,69 +1,82 @@
-var langs =
-[['Afrikaans',       ['af-ZA']],
- ['Bahasa Indonesia',['id-ID']],
- ['Bahasa Melayu',   ['ms-MY']],
- ['Català',          ['ca-ES']],
- ['Čeština',         ['cs-CZ']],
- ['Deutsch',         ['de-DE']],
- ['English',         ['en-AU', 'Australia'],
-                     ['en-CA', 'Canada'],
-                     ['en-IN', 'India'],
-                     ['en-NZ', 'New Zealand'],
-                     ['en-ZA', 'South Africa'],
-                     ['en-GB', 'United Kingdom'],
-                     ['en-US', 'United States']],
- ['Español',         ['es-AR', 'Argentina'],
-                     ['es-BO', 'Bolivia'],
-                     ['es-CL', 'Chile'],
-                     ['es-CO', 'Colombia'],
-                     ['es-CR', 'Costa Rica'],
-                     ['es-EC', 'Ecuador'],
-                     ['es-SV', 'El Salvador'],
-                     ['es-ES', 'España'],
-                     ['es-US', 'Estados Unidos'],
-                     ['es-GT', 'Guatemala'],
-                     ['es-HN', 'Honduras'],
-                     ['es-MX', 'México'],
-                     ['es-NI', 'Nicaragua'],
-                     ['es-PA', 'Panamá'],
-                     ['es-PY', 'Paraguay'],
-                     ['es-PE', 'Perú'],
-                     ['es-PR', 'Puerto Rico'],
-                     ['es-DO', 'República Dominicana'],
-                     ['es-UY', 'Uruguay'],
-                     ['es-VE', 'Venezuela']],
- ['Euskara',         ['eu-ES']],
- ['Français',        ['fr-FR']],
- ['Galego',          ['gl-ES']],
- ['Hrvatski',        ['hr_HR']],
- ['IsiZulu',         ['zu-ZA']],
- ['Íslenska',        ['is-IS']],
- ['Italiano',        ['it-IT', 'Italia'],
-                     ['it-CH', 'Svizzera']],
- ['Magyar',          ['hu-HU']],
- ['Nederlands',      ['nl-NL']],
- ['Norsk bokmål',    ['nb-NO']],
- ['Polski',          ['pl-PL']],
- ['Português',       ['pt-BR', 'Brasil'],
-                     ['pt-PT', 'Portugal']],
- ['Română',          ['ro-RO']],
- ['Slovenčina',      ['sk-SK']],
- ['Suomi',           ['fi-FI']],
- ['Svenska',         ['sv-SE']],
- ['Türkçe',          ['tr-TR']],
- ['български',       ['bg-BG']],
- ['Pусский',         ['ru-RU']],
- ['Српски',          ['sr-RS']],
- ['한국어',            ['ko-KR']],
- ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
-                     ['cmn-Hans-HK', '普通话 (香港)'],
-                     ['cmn-Hant-TW', '中文 (台灣)'],
-                     ['yue-Hant-HK', '粵語 (香港)']],
- ['日本語',           ['ja-JP']],
- ['Lingua latīna',   ['la']]];
+let assert = require("assert");
+let pythonBridge = require("python-bridge");
 
-var select_language = document.getElementById('select_language');
-var select_dialect = document.getElementById('select_dialect');
+var langs = [
+  ["Afrikaans", ["af-ZA"]],
+  ["Bahasa Indonesia", ["id-ID"]],
+  ["Bahasa Melayu", ["ms-MY"]],
+  ["Català", ["ca-ES"]],
+  ["Čeština", ["cs-CZ"]],
+  ["Deutsch", ["de-DE"]],
+  [
+    "English",
+    ["en-AU", "Australia"],
+    ["en-CA", "Canada"],
+    ["en-IN", "India"],
+    ["en-NZ", "New Zealand"],
+    ["en-ZA", "South Africa"],
+    ["en-GB", "United Kingdom"],
+    ["en-US", "United States"],
+  ],
+  [
+    "Español",
+    ["es-AR", "Argentina"],
+    ["es-BO", "Bolivia"],
+    ["es-CL", "Chile"],
+    ["es-CO", "Colombia"],
+    ["es-CR", "Costa Rica"],
+    ["es-EC", "Ecuador"],
+    ["es-SV", "El Salvador"],
+    ["es-ES", "España"],
+    ["es-US", "Estados Unidos"],
+    ["es-GT", "Guatemala"],
+    ["es-HN", "Honduras"],
+    ["es-MX", "México"],
+    ["es-NI", "Nicaragua"],
+    ["es-PA", "Panamá"],
+    ["es-PY", "Paraguay"],
+    ["es-PE", "Perú"],
+    ["es-PR", "Puerto Rico"],
+    ["es-DO", "República Dominicana"],
+    ["es-UY", "Uruguay"],
+    ["es-VE", "Venezuela"],
+  ],
+  ["Euskara", ["eu-ES"]],
+  ["Français", ["fr-FR"]],
+  ["Galego", ["gl-ES"]],
+  ["Hrvatski", ["hr_HR"]],
+  ["IsiZulu", ["zu-ZA"]],
+  ["Íslenska", ["is-IS"]],
+  ["Italiano", ["it-IT", "Italia"], ["it-CH", "Svizzera"]],
+  ["Magyar", ["hu-HU"]],
+  ["Nederlands", ["nl-NL"]],
+  ["Norsk bokmål", ["nb-NO"]],
+  ["Polski", ["pl-PL"]],
+  ["Português", ["pt-BR", "Brasil"], ["pt-PT", "Portugal"]],
+  ["Română", ["ro-RO"]],
+  ["Slovenčina", ["sk-SK"]],
+  ["Suomi", ["fi-FI"]],
+  ["Svenska", ["sv-SE"]],
+  ["Türkçe", ["tr-TR"]],
+  ["български", ["bg-BG"]],
+  ["Pусский", ["ru-RU"]],
+  ["Српски", ["sr-RS"]],
+  ["한국어", ["ko-KR"]],
+  [
+    "中文",
+    ["cmn-Hans-CN", "普通话 (中国大陆)"],
+    ["cmn-Hans-HK", "普通话 (香港)"],
+    ["cmn-Hant-TW", "中文 (台灣)"],
+    ["yue-Hant-HK", "粵語 (香港)"],
+  ],
+  ["日本語", ["ja-JP"]],
+  ["Lingua latīna", ["la"]],
+];
+
+let python = pythonBridge();
+
+var select_language = document.getElementById("select_language");
+var select_dialect = document.getElementById("select_dialect");
 
 for (var i = 0; i < langs.length; i++) {
   select_language.options[i] = new Option(langs[i][0], i);
@@ -71,7 +84,7 @@ for (var i = 0; i < langs.length; i++) {
 select_language.selectedIndex = 6;
 updateCountry();
 select_dialect.selectedIndex = 6;
-showInfo('info_start');
+showInfo("info_start");
 
 function updateCountry() {
   for (var i = select_dialect.options.length - 1; i >= 0; i--) {
@@ -81,64 +94,64 @@ function updateCountry() {
   for (var i = 1; i < list.length; i++) {
     select_dialect.options.add(new Option(list[i][1], list[i][0]));
   }
-  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+  select_dialect.style.visibility = list[1].length == 1 ? "hidden" : "visible";
 }
 
 var create_email = false;
-var final_transcript = '';
+var final_transcript = "";
 var recognizing = false;
 var ignore_onend;
 var start_timestamp;
-if (!('webkitSpeechRecognition' in window)) {
+if (!("webkitSpeechRecognition" in window)) {
   upgrade();
 } else {
-  start_button.style.display = 'inline-block';
+  start_button.style.display = "inline-block";
   var recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
   recognition.interimResults = true;
 
-  recognition.onstart = function() {
+  recognition.onstart = function () {
     recognizing = true;
-    showInfo('info_speak_now');
+    showInfo("info_speak_now");
     // start_img.src = 'mic.png';
   };
 
-  recognition.onerror = function(event) {
-    if (event.error == 'no-speech') {
-    //   start_img.src = 'mic.png';
-      showInfo('info_no_speech');
+  recognition.onerror = function (event) {
+    if (event.error == "no-speech") {
+      //   start_img.src = 'mic.png';
+      showInfo("info_no_speech");
       ignore_onend = true;
     }
-    if (event.error == 'audio-capture') {
-    //   start_img.src = 'mic.png';
-      showInfo('info_no_microphone');
+    if (event.error == "audio-capture") {
+      //   start_img.src = 'mic.png';
+      showInfo("info_no_microphone");
       ignore_onend = true;
     }
-    if (event.error == 'not-allowed') {
+    if (event.error == "not-allowed") {
       if (event.timeStamp - start_timestamp < 100) {
-        showInfo('info_blocked');
+        showInfo("info_blocked");
       } else {
-        showInfo('info_denied');
+        showInfo("info_denied");
       }
       ignore_onend = true;
     }
   };
 
-  recognition.onend = function() {
+  recognition.onend = function () {
     recognizing = false;
     if (ignore_onend) {
       return;
     }
     // start_img.src = 'mic.png';
     if (!final_transcript) {
-      showInfo('info_start');
+      showInfo("info_start");
       return;
     }
-    showInfo('');
+    showInfo("");
     if (window.getSelection) {
       window.getSelection().removeAllRanges();
       var range = document.createRange();
-      range.selectNode(document.getElementById('final_span'));
+      range.selectNode(document.getElementById("final_span"));
       window.getSelection().addRange(range);
     }
     if (create_email) {
@@ -147,8 +160,8 @@ if (!('webkitSpeechRecognition' in window)) {
     }
   };
 
-  recognition.onresult = function(event) {
-    var interim_transcript = '';
+  recognition.onresult = function (event) {
+    var interim_transcript = "";
     for (var i = event.resultIndex; i < event.results.length; ++i) {
       if (event.results[i].isFinal) {
         final_transcript += event.results[i][0].transcript;
@@ -160,35 +173,37 @@ if (!('webkitSpeechRecognition' in window)) {
     final_span.innerHTML = linebreak(final_transcript);
     interim_span.innerHTML = linebreak(interim_transcript);
     if (final_transcript || interim_transcript) {
-      showButtons('inline-block');
+      showButtons("inline-block");
     }
   };
 }
 
 function upgrade() {
-  start_button.style.visibility = 'hidden';
-  showInfo('info_upgrade');
+  start_button.style.visibility = "hidden";
+  showInfo("info_upgrade");
 }
 
 var two_line = /\n\n/g;
 var one_line = /\n/g;
 function linebreak(s) {
-  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+  return s.replace(two_line, "<p></p>").replace(one_line, "<br>");
 }
 
 var first_char = /\S/;
 function capitalize(s) {
-  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+  return s.replace(first_char, function (m) {
+    return m.toUpperCase();
+  });
 }
 
 function createEmail() {
-  var n = final_transcript.indexOf('\n');
+  var n = final_transcript.indexOf("\n");
   if (n < 0 || n >= 80) {
-    n = 40 + final_transcript.substring(40).indexOf(' ');
+    n = 40 + final_transcript.substring(40).indexOf(" ");
   }
   var subject = encodeURI(final_transcript.substring(0, n));
   var body = encodeURI(final_transcript.substring(n + 1));
-  window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+  window.location.href = "mailto:?subject=" + subject + "&body=" + body;
 }
 
 function copyButton() {
@@ -196,30 +211,27 @@ function copyButton() {
     recognizing = false;
     recognition.stop();
   }
-  copy_button.style.display = 'none';
-  copy_info.style.display = 'inline-block';
-  showInfo('');
+  copy_button.style.display = "none";
+  copy_info.style.display = "inline-block";
+  showInfo("");
 }
 
 // save to json
 var iterator = 0;
-const fs = require("fs");
 function nextButton() {
-    // if (recognizing) {
-    //   recognizing = false;
-    //   recognition.stop();
-    // }
-    console.log("next image requested");
-    console.log([final_transcript]);
-    var myJsonString = JSON.stringify([final_transcript]);
-    fs.writeFile("user_input.json", myJsonString, (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log("JSON data is saved.");
-    });
-    newImg();
-  }
+  // if (recognizing) {
+  //   recognizing = false;
+  //   recognition.stop();
+  // }
+  console.log("next image requested");
+  console.log([final_transcript]);
+  // use python
+  python.ex`import math`;
+
+  //use model
+  
+  newImg();
+}
 var testImages = [
   "https://images.ctfassets.net/81iqaqpfd8fy/3r4flvP8Z26WmkMwAEWEco/870554ed7577541c5f3bc04942a47b95/78745131.jpg?w=1200&h=1200&fm=jpg&fit=fill",
   "https://upload.wikimedia.org/wikipedia/commons/c/cb/The_Blue_Marble_%28remastered%29.jpg",
@@ -230,10 +242,12 @@ var testImages = [
   "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/high-angle-view-of-variety-of-succulent-plants-royalty-free-image-1584462052.jpg",
   "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p.jpg",
   "https://studio.knightlab.com/assets/people/kris-hammond.jpg",
-]
+];
 function newImg() {
   let index = Math.floor(Math.random() * testImages.length);
-  document.getElementById("images").innerHTML = `<img id="new_img" src="${testImages[index]}">`;
+  document.getElementById(
+    "images"
+  ).innerHTML = `<img id="new_img" src="${testImages[index]}">`;
 }
 
 function emailButton() {
@@ -244,9 +258,9 @@ function emailButton() {
   } else {
     createEmail();
   }
-  email_button.style.display = 'none';
-  email_info.style.display = 'inline-block';
-  showInfo('');
+  email_button.style.display = "none";
+  email_info.style.display = "inline-block";
+  showInfo("");
 }
 
 function startButton(event) {
@@ -254,15 +268,15 @@ function startButton(event) {
     recognition.stop();
     return;
   }
-  final_transcript = '';
+  final_transcript = "";
   recognition.lang = select_dialect.value;
   recognition.start();
   ignore_onend = false;
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-//   start_img.src = 'mic.png';
-  showInfo('info_allow');
-  showButtons('none');
+  final_span.innerHTML = "";
+  interim_span.innerHTML = "";
+  //   start_img.src = 'mic.png';
+  showInfo("info_allow");
+  showButtons("none");
   start_timestamp = event.timeStamp;
 }
 
@@ -270,12 +284,12 @@ function showInfo(s) {
   if (s) {
     for (var child = info.firstChild; child; child = child.nextSibling) {
       if (child.style) {
-        child.style.display = child.id == s ? 'inline' : 'none';
+        child.style.display = child.id == s ? "inline" : "none";
       }
     }
-    info.style.visibility = 'visible';
+    info.style.visibility = "visible";
   } else {
-    info.style.visibility = 'hidden';
+    info.style.visibility = "hidden";
   }
 }
 
@@ -285,10 +299,10 @@ function showButtons(style) {
     return;
   }
   current_style = style;
-//   copy_button.style.display = style;
-//   email_button.style.display = style;
-//   next_button.style.display = style;
-//   copy_info.style.display = 'none';
-//   email_info.style.display = 'none';
-//   email_info.style.display = 'none';
+  //   copy_button.style.display = style;
+  //   email_button.style.display = style;
+  //   next_button.style.display = style;
+  //   copy_info.style.display = 'none';
+  //   email_info.style.display = 'none';
+  //   email_info.style.display = 'none';
 }
