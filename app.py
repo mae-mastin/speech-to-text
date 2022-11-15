c1e56bc96ced09031f789098671db7667fbebfc2
Chenwei1999TZ <73069964+Chenwei-1999@users.noreply.github.com>
2022-11-15 11:19:29 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import json
-from generator import generate_score
+from generator import get_image
+from PIL import Image
+import io
+import base64
 
 # Creating the flask object
 app = Flask(__name__)
@@ -13,21 +16,27 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/', methods=["GET", "POST"])
 #@app.route('/index', methods=["GET", "POST"])
 def index():
+
     if request.method == "POST":
         jsonData = request.get_json()
-        print(jsonData)
-        #with open("sample.json", "w") as outfile:
-         #   outfile.write(jsonData)
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-    return render_template("record.html")
+        print('new_image')
+        # # print(jsonData)
+        get_image(jsonData)
 
-@app.route('/test/<string:final_transcript>', methods=['POST'])
-def test(final_transcript):
-    final_transcript = json.loads(final_transcript)
-    return generate_score(final_transcript)
+    im = Image.open('static/image.jpg')
+    data = io.BytesIO()
+    im.save(data, 'JPEG')
+    encoded_img_data = base64.b64encode(data.getvalue())
+    #with open("sample.json", "w") as outfile:
+        #   outfile.write(jsonData)
+
+       # return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return render_template("record.html", img_data=encoded_img_data.decode('utf-8'))
+
 
 # When this Python script is called this command is called too, to actively
 # host the routes on a local server, to change the port (like 5000 or 3000)
 # run app.run(debug=True, port = 3000), app.run MUST be placed at the end of
 # the script
-app.run(debug=True, port=3000,threaded=True)
+if __name__ == '__main__':
+    app.run()
--- generator.py
+++ generator.py
@@ -8,39 +8,39 @@ import numpy as np
 import clip
 from PIL import Image
 import requests
-import json
+import pickle
 
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda:0')  
 
-def generate_score(data):
+with open('embeddings.pkl', 'rb') as f:
+    embeddings = pickle.load(f)
 
+stackembeddings = []
 
-    device = torch.device('cpu')
-    if torch.cuda.is_available():
-        device = torch.device('cuda:0')    
+for i in range(10):
+    stackembeddings.append(torch.cat(embeddings[i]))
 
-    transform = transforms.Compose([
-            transforms.Resize(224),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+stackembeddings = torch.cat(stackembeddings)
 
-    data_viz = torchvision.datasets.ImageFolder(
-        "Visualize",
-        transform=transform
-    )
+transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+])
 
-    viz_loader =  torch.utils.data.DataLoader(
-        data_viz,
-        batch_size=1, 
-    )
+model, preprocess = clip.load("ViT-B/32", device=device)
+    
+data_viz = torchvision.datasets.ImageFolder(
+    "raw-img",
+    transform=transform
+)
 
-    model, preprocess = clip.load("ViT-B/32", device=device)
-
-    text = clip.tokenize(data).to(device)
-    image = preprocess(Image.open("ßgolden_retriever.jpg")).unsqueeze(0).to(device)
-
-    logits_per_image, logits_per_text = model(image, text)
-    probs = logits_per_image.softmax(dim=-1).detach().cpu().numpy()
-
-    return probs
+def get_image(text):
+    text=clip.tokenize(text).to(device)
+    text_features = model.encode_text(text)
+    scores = torch.einsum('ab, cb -> ac', stackembeddings, text_features)
+    idx = torch.argmax(scores)
+    pilImage = Image.fromarray((np.transpose(data_viz[idx][0], (1,2,0)).numpy() * 255).astype(np.uint8))
+    pilImage.save('static/image.jpg')
